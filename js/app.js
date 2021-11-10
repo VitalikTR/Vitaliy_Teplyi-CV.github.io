9283463b20e4652daadd60d4cd8115722d277f16
Vitalik <Vitaliktr@gmail.com>
2021-11-10 11:48:19 +0200

    added modal and fixed modal js

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//------modal-------
+
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".btn")
+
+function openModal() {
+    modal.classList.add("show")
+        modal.classList.remove("hide")
+}
+
+function closeModal() {
+    modal.classList.add("hide")
+        modal.classList.remove("show")
+}
+
+function openModalByScroll() {
+    if(window.pageYOffset >= document.documentElement.scrollHeight/2 || scroll) {
+        openModal()
+        window.removeEventListener('scroll', openModalByScroll)
+    }
+}
+
+window.addEventListener('scroll', openModalByScroll)
+
+closeBtn.addEventListener("click",closeModal);
+    modal.addEventListener("click", function(e){
+        if (e.target === modal) {
+            closeModal()
+    }
+})
